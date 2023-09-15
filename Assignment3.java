9c6086f141adaafbd585d52fb067ff67ed67dca3
Thien Nguyen <thiennguyen78956@yahoo.com>
2023-09-15 01:07:47 -0500

    Implemented a main method and added a scanner

--- Assignment3.java
+++ Assignment3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public static int arraySum(int[] array) {
 	    
     int sum = 0;
@@ -16,4 +18,26 @@ public static int arrayProduct(int[] array) {
     }
     
     return product;
+}
+
+public static void main(String args[]) {
+	    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the number of elements for the array: ");
+    int num = scanner.nextInt();
+    int[] array = new int[num];
+    
+    System.out.println("Enter" + num + "numbers:");
+    for (int i = 0; i < num; i++) {
+        System.out.println("Element " + (i + 1) + ":");
+        array[i]=scanner.nextInt();
+    }
+    
+    int totalSum = arraySum(array);
+    int totalProduct = arrayProduct(array);
+    
+    System.out.println("The sum of the array is :" + totalSum);
+    System.out.println("The product of the array is:" + totalProduct);
+
+    scanner.close();
 }
