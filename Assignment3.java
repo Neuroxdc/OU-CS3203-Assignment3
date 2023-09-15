1019c2f6eb6da05535a836002f9e1b4c96823f49
Thien Nguyen <thiennguyen78956@yahoo.com>
2023-09-15 02:12:25 -0500

    Added a class

--- Assignment3.java
+++ Assignment3.java
@@ -1,43 +1,46 @@
 import java.util.Scanner;
 
-public static int arraySum(int[] array) {
+public class Assignment3 {
+    public static int arraySum(int[] array) {
 	    
-    int sum = 0;
-    for (int i = 0; i < array.length; i++) {
-        sum += array[i];
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        
+        return sum;
     }
     
-    return sum;
-}
-
-public static int arrayProduct(int[] array) {
-	    
-    int product = 1;
-    for (int i = 0; i < array.length; i++) {
-        product *= array[i];
+    public static int arrayProduct(int[] array) {
+            
+        int product = 1;
+        for (int i = 0; i < array.length; i++) {
+            product *= array[i];
+        }
+        
+        return product;
     }
     
-    return product;
-}
-
-public static void main(String args[]) {
-	    
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the number of elements for the array: ");
-    int num = scanner.nextInt();
-    int[] array = new int[num];
+    public static void main(String args[]) {
+            
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements for the array: ");
+        int num = scanner.nextInt();
+        int[] array = new int[num];
+        
+        System.out.println("Enter" + num + "numbers:");
+        for (int i = 0; i < num; i++) {
+            System.out.println("Element " + (i + 1) + ":");
+            array[i]=scanner.nextInt();
+        }
+        
+        int totalSum = arraySum(array);
+        int totalProduct = arrayProduct(array);
+        
+        System.out.println("The sum of the array is :" + totalSum);
+        System.out.println("The product of the array is:" + totalProduct);
     
-    System.out.println("Enter" + num + "numbers:");
-    for (int i = 0; i < num; i++) {
-        System.out.println("Element " + (i + 1) + ":");
-        array[i]=scanner.nextInt();
+        scanner.close();
     }
     
-    int totalSum = arraySum(array);
-    int totalProduct = arrayProduct(array);
-    
-    System.out.println("The sum of the array is :" + totalSum);
-    System.out.println("The product of the array is:" + totalProduct);
-
-    scanner.close();
 }
